0a80765be4cc29fb4679941b2c6a144b2db3ba99
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-04-26 11:36:20 +0300

    new file:   .minerId 	modified:   .token 	modified:   cli.js 	deleted:    jobs/7Mxxz2WuGdM1Hw5aqG9p/details.txt 	deleted:    jobs/7Mxxz2WuGdM1Hw5aqG9p/script.py 	deleted:    jobs/7Mxxz2WuGdM1Hw5aqG9p/validationData.xlsx 	deleted:    jobs/9z4kNEXfWYuMW7wDhrih/details.txt 	deleted:    jobs/9z4kNEXfWYuMW7wDhrih/script.py 	deleted:    jobs/9z4kNEXfWYuMW7wDhrih/trainingData.xlsx 	deleted:    jobs/9z4kNEXfWYuMW7wDhrih/validationData.xlsx 	modified:   package-lock.json 	modified:   package.json

--- cli.js
+++ cli.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 const axios = require('axios');
+const si = require('systeminformation');
 const yargs = require('yargs/yargs')(process.argv.slice(2));
 const { hideBin } = require('yargs/helpers');
 require('dotenv').config();
-  const fs = require('fs');
-  const path = require('path');
+const fs = require('fs');
+const path = require('path');
+
+let inquirer;
 
 const apiUrl = process.env.API_URL;
 
@@ -22,27 +25,97 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-  const handleLogin = async () => {
-      const { username, password } = argv;
-      try {
-          const response = await axios.post(`${apiUrl}/login`, { username, password });
-          console.log('Login successful. Token received.');
-          fs.writeFileSync(path.join(__dirname, '.token'), response.data.token);
-      } catch (error) {
-          console.error('Login failed:', error.response?.data?.error || 'Server error');
-      }
-  };
-
-const handleLogout = () => {
+async function getSystemDetails() {
     try {
-        fs.unlinkSync(path.join(__dirname, '.token'));  // This deletes the token file
-        console.log('Logged out successfully. Token removed.');
+        const cpu = await si.cpu();
+        const graphics = await si.graphics();
+        const networkInterfaces = await si.networkInterfaces();
+        const osInfo = await si.osInfo();
+
+        return {
+            cpu: {
+                brand: cpu.brand,
+                cores: cpu.cores,
+                manufacturer: cpu.manufacturer,
+                physicalCores: cpu.physicalCores,
+                speed: cpu.speed
+            },
+            graphics: graphics.controllers.map(gpu => ({
+                model: gpu.model,
+                vram: gpu.vram
+            })),
+            network: networkInterfaces.map(iface => ({
+                iface: iface.ifaceName,
+                ip4: iface.ip4,
+                mac: iface.mac
+            })),
+            os: {
+                distro: osInfo.distro,
+                platform: osInfo.platform,
+                release: osInfo.release
+            }
+        };
+    } catch (error) {
+        console.error('Failed to get system details:', error);
+        return {};
+    }
+}
+
+const handleRegisterMiner = async () => {
+    const { ethereumAddress, username, email } = argv;
+    try {
+        const response = await axios.post(`${apiUrl}/register-miner`, { ethereumAddress, username, email });
+        console.log('Registration successful:', response.data);
+    } catch (error) {
+        console.error('Registration failed:', error.response?.data?.error || 'Server error');
+    }
+};
+
+const handleLogin = async () => {
+    const { username, password } = argv;
+    console.log("Username $ password: ", username, password);
+    try {
+        const response = await axios.post(`${apiUrl}/login`, { username, password });
+        
+        // Write the token
+        fs.writeFileSync(path.join(__dirname, '.token'), response.data.token);
+
+        // Ensure minerId is a string and write it
+        if (response.data.minerId) {
+            fs.writeFileSync(path.join(__dirname, '.minerId'), response.data.minerId.toString());
+        } else {
+            console.error('Miner ID is missing in the response.');
+        }
+
+        console.log('Login successful. Token and Miner ID received.');
+    } catch (error) {
+        console.error('Login failed:', error.response?.data?.error || error);
+    }
+};
+
+  const handleLogout = () => {
+    try {
+        // Remove the authentication token file
+        const tokenPath = path.join(__dirname, '.token');
+        if (fs.existsSync(tokenPath)) {
+            fs.unlinkSync(tokenPath);
+            console.log('Authentication token removed.');
+        }
+
+        // Remove the jobs directory and all its contents
+        const jobsDir = path.join(__dirname, 'jobs');
+        if (fs.existsSync(jobsDir)) {
+            fs.rmdirSync(jobsDir, { recursive: true });
+            console.log('Job data directory removed.');
+        }
+
+        console.log('Logged out successfully.');
     } catch (error) {
         console.error('Logout failed:', error.message);
     }
 };
-  
-  const getToken = () => {
+
+const getToken = () => {
     try {
         return fs.readFileSync(path.join(__dirname, '.token'), 'utf8');
     } catch (error) {
@@ -50,6 +123,16 @@ const handleLogout = () => {
         return null;
     }
 };
+
+const getMinerId = () => {
+    try {
+        return fs.readFileSync(path.join(__dirname, '.minerId'), 'utf8');
+    } catch (error) {
+        console.error('Error reading miner ID:', error.message);
+        return null;
+    }
+};
+
 
 const handleFetchPendingJobs = async () => {
     const token = getToken();
@@ -74,9 +157,47 @@ const handleFetchJobDetails = async () => {
     }
     try {
         const response = await axios.get(`${apiUrl}/job-details/${docId}`, { headers: { Authorization: `Bearer ${token}` } });
-        const jobDetails = response.data;
+        console.log('Job details:', response.data);
+    } catch (error) {
+        console.error('Failed to fetch job details:', error.response?.data?.error || 'Server error');
+    }
+};
+
+const start_training = async () => {
+    const token = getToken();
+    const minerId = getMinerId();
+    console.log('Gotten miner id: ', minerId);
+    const systemDetails = await getSystemDetails();
+    const { docId } = argv;
+    if (!token) {
+        console.log('Authentication token not found. Please login first.');
+        return;
+    }
+
+    // Confirmation prompt
+    const answers = await inquirer.prompt([{
+        type: 'confirm',
+        name: 'confirmTraining',
+        message: 'Starting training will trigger execution of the training job. Do you want to continue?',
+        default: false
+    }]);
+
+    if (!answers.confirmTraining) {
+        console.log('Training start cancelled.');
+        return;
+    }
+
+    try {
+        const response = await axios.post(`${apiUrl}/start-training/${docId}`, {
+            systemDetails,
+            minerId
+        }, {
+            headers: { Authorization: `Bearer ${token}`, 
+            'X-Miner-ID': minerId  }
+        });
+        const train_job = response.data;
         console.log('Job details fetched successfully. See details below:');
-        console.log(jobDetails);
+        console.log(train_job);
 
         const jobDir = path.join(__dirname, 'jobs', docId);
         if (!fs.existsSync(jobDir)) {
@@ -84,26 +205,26 @@ const handleFetchJobDetails = async () => {
         }
 
         const detailsPath = path.join(jobDir, 'details.txt');
-        fs.writeFileSync(detailsPath, JSON.stringify(jobDetails, null, 2));
+        fs.writeFileSync(detailsPath, JSON.stringify(train_job, null, 2));
         console.log(`Job details saved to: ${detailsPath}`);
 
         // Download files using the URLs provided
-        if (jobDetails.scriptUrl) {
-            await downloadFile(jobDetails.scriptUrl, path.join(jobDir, 'script.py'));
+        if (train_job.scriptUrl) {
+            await downloadFile(train_job.scriptUrl, path.join(jobDir, 'script.py'));
             console.log(`Script file downloaded to: ${path.join(jobDir, 'script.py')}`);
         }
-        if (jobDetails.trainingFileUrl) {
-            await downloadFile(jobDetails.trainingFileUrl, path.join(jobDir, 'trainingData.xlsx'));
+        if (train_job.trainingFileUrl) {
+            await downloadFile(train_job.trainingFileUrl, path.join(jobDir, 'trainingData.xlsx'));
             console.log(`Training data file downloaded to: ${path.join(jobDir, 'trainingData.xlsx')}`);
         }
-        if (jobDetails.validationFileUrl) {
-            await downloadFile(jobDetails.validationFileUrl, path.join(jobDir, 'validationData.xlsx'));
+        if (train_job.validationFileUrl) {
+            await downloadFile(train_job.validationFileUrl, path.join(jobDir, 'validationData.xlsx'));
             console.log(`Validation data file downloaded to: ${path.join(jobDir, 'validationData.xlsx')}`);
         }
 
         console.log(`Training job fetched successfully. Check directory: ${jobDir}`);
     } catch (error) {
-        console.error('Failed to fetch job details:', error.response?.data?.error || 'Server error');
+        console.error('Failed to fetch job details:', error.response?.data?.error || error);
     }
 };
 
@@ -128,23 +249,32 @@ const downloadFile = async (fileUrl, outputPath) => {
 };
 
 // Execute command based on user input
-switch (argv.command) {
-    case 'login': 
-        handleLogin(); 
-        break;
-    case 'register-miner': 
-        handleRegisterMiner(); 
-        break;
-    case 'fetch-pending-jobs': 
-        handleFetchPendingJobs(); 
-        break;
-    case 'fetch-job-details': 
-        handleFetchJobDetails(); 
-        break;
-    case 'logout': 
-        handleLogout(); 
-        break;
-    default: 
-        console.log('Invalid command');
-        break;
-}
+(async () => {
+    // Load inquirer dynamically
+    inquirer = (await import('inquirer')).default;
+
+    // Execute command based on user input
+    switch (argv.command) {
+        case 'login': 
+            await handleLogin(); 
+            break;
+        case 'register-miner': 
+            await handleRegisterMiner(); 
+            break;
+        case 'fetch-pending-jobs': 
+            await handleFetchPendingJobs(); 
+            break;
+        case 'fetch-job-details': 
+            await handleFetchJobDetails(); 
+            break;
+        case 'train-jobs': 
+            await start_training(); 
+            break;
+        case 'logout': 
+            handleLogout(); 
+            break;
+        default: 
+            console.log('Invalid command');
+            break;
+    }
+})();
